e44c3acf7a05ff383331682eb1b330917b2d6774
Cristian David Velazquez Ramirez <cdvr1993@gmail.com>
2018-10-18 16:32:54 -0700

    Remove unnecesary if

--- services/user.go
+++ services/user.go
@@ -1,7 +1,6 @@
 package services
 
 import (
-	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/orm"
 	"github.com/cdvr1993/deployment-manager/models"
 )
@@ -34,8 +33,6 @@ func (s UserService) AddUser(u *models.User) {
 		} else {
 			panic(ErrorUserEmailExists(u.Email))
 		}
-	} else {
-		panic(err)
 	}
 }
 
@@ -46,9 +43,6 @@ func (s UserService) GetUser(e string) (user models.User) {
 
 	if err := o.Read(&user, "email"); err == orm.ErrNoRows {
 		panic(ErrorUserNotFound(e))
-	} else if err != nil {
-		beego.Error(err)
-		panic(err)
 	}
 
 	return
