2375c35ba79194532f48141f8fa034b2fad7fc71
Cristian David Velazquez Ramirez <cdvr1993@gmail.com>
2018-10-18 11:46:13 -0700

    Improve error handler
    	If the error is unknown:
    		1. Create a transaction id.
    		2. Log the error.
    		3. Return the transaction id to the user.

--- services/response.go
+++ services/response.go
@@ -1,7 +1,10 @@
 package services
 
 import (
+	"fmt"
+
 	"github.com/astaxie/beego"
+	"github.com/satori/go.uuid"
 )
 
 func ServeJson(c *beego.Controller) {
@@ -9,6 +12,22 @@ func ServeJson(c *beego.Controller) {
 		if svc, ok := r.(IServiceError); ok {
 			c.Ctx.ResponseWriter.WriteHeader(svc.Status())
 			c.Data["json"] = map[string][]string{"errors": []string{svc.Error()}}
+		} else {
+			// Create an unique id to make a relationship between
+			// API error and log error
+			var u1String string
+
+			if u1, err := uuid.NewV4(); err == nil {
+				u1String = fmt.Sprintf("%s", u1)
+			} else {
+				u1String = "Unknown"
+			}
+
+			beego.Error(fmt.Sprintf("%s - %s", u1String, r))
+			c.Ctx.ResponseWriter.WriteHeader(500)
+			c.Data["json"] = map[string][]string{
+				"errors": []string{fmt.Sprintf("Transaction: '%s'", u1String)},
+			}
 		}
 	}
 	c.ServeJSON()
